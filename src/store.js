a816f35cb8fd261de7544c11405f1ef5010b4a19
danportchen <danport.chen@gmail.com>
2018-09-06 18:56:44 +0800

    pratice ok

--- src/store.js
+++ src/store.js
@@ -3,8 +3,45 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+const LESSONS_URL =
+  "https://hiskio.com/api/v1/courses/professions?type=all&level=all&sort=latest&profession_id=1";
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    title: "My Custom Title",
+    lessons: [],
+    cart: []
+  },
+  getters: {
+    lessonsInCart: state => (
+      state.cart.length
+    ),
+    priceInCart: state => (
+      state.cart.reduce((acc, lesson) => (
+        acc + lesson.price
+      ), 0)
+    ),
+    isLessonInCart: state => lesson => (
+       state.cart.findIndex(({ id }) => (
+           id === lesson.id
+        )) !== -1
+    ),
+  },
+  mutations: {
+    setLessons(state, lessons) {
+      state.lessons = lessons;
+    },
+    addToCart(state, lesson) {
+      state.cart = state.cart.concat(lesson);
+    },
+  },
+  actions: {
+    fetchLessons({ commit }) {
+      fetch(LESSONS_URL)
+        .then(rs => rs.json())
+        .then(rs => {
+          commit("setLessons", rs.courses);
+        });
+    }
+  }
 });
---
+++ src/views/LessonList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="list">
+    <LessonItems v-for="lesson in lessons"  :lesson="lesson" :key="lesson.id"/>
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex';
+import LessonItems from './LessonItems';
+
+  export default {
+    components:{
+      LessonItems,
+    },
+    computed: {
+      ...mapState([ 'lessons' ]),
+    },
+    methods: {
+      ...mapActions(['fetchLessons']),
+    },
+    mounted() {
+      this.fetchLessons();
+    },
+  };
+</script>
+
+<style scoped>
+ .list {
+   padding-top: 40px;
+   text-align: center;
+ }
+</style>
